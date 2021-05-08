01561e407e3ccc34c8c09349aacaa190b0c82fbf
AngelinaRajesh <>
2021-05-08 19:21:42 +0000

    spacing

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ let userGuess = 0
 function randomizer () {
   // get number typed in by user and convert to an integer
   userGuess = document.getElementById('input').value
-  userGuess = parseInt (userGuess)
+  userGuess = parseInt(userGuess)
   // generate random number between 1 and 6
   randomNumber = (Math.random() * 6) + 1
-  randomNumber = parseInt (randomNumber)
+  randomNumber = parseInt(randomNumber)
   // Compare user's guess with random number
   if (userGuess === randomNumber) {
     document.getElementById('answer').innerHTML = 'You Win!'
